81e17e6112a4be33deaa9cc716ba395b983ac206
yuenkaifai <yuenkaifai@realtek-sg.com>
2025-12-04 11:38:05 +0800

    Update sensor.h
    -Fix compile error

--- project/realtek_amebapro2_v0_example/inc/sensor.h
+++ project/realtek_amebapro2_v0_example/inc/sensor.h
@@ -156,10 +156,10 @@ static const struct sensor_params_t sensor_params[] = {
 	[SENSOR_OS02H10]          = {1920, 1080, 30},
 	[SENSOR_OV13B10]  	      = {2096,  1560, 30},
 	[SENSOR_OV13B10_12M]      = {4032,  3024, 10},
-	[SENSOR_OV13B10_12M_SEQ]  = {2032,  1532, 5} //width = 2016 + 16(overlap) //height = 1512 + 20
+	[SENSOR_OV13B10_12M_SEQ]  = {2032,  1532, 5}, //width = 2016 + 16(overlap) //height = 1512 + 20
 	[SENSOR_OV12890]  	      = {2048,  1536, 30},
 	[SENSOR_OV12890_12M]      = {4032,  3024, 8},
-	[SENSOR_OV12890_12M_SEQ]  = {2032,  1532, 5}, //width = 2016 + 16(overlap) //height = 1512 + 20
+	[SENSOR_OV12890_12M_SEQ]  = {2032,  1532, 5} //width = 2016 + 16(overlap) //height = 1512 + 20
 };
 
 #define SENSOR_MAX         4
